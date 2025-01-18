43389e86c8f7fa473f76346243068c9100b3ee6e
Peter Juhasz <juhasz.peter1337@gmail.com>
2025-01-18 17:01:25 +0100

    Added arrow function example

---
+++ arrow_function.js
@@ -0,0 +1,11 @@
+const add = (number1, number2) => number1 + number2;
+
+const multiple_by_2 = number => number * 2;
+
+const divide_by_2 = number => number / 2;
+
+const number = 20;
+
+console.log(add(2, 3));
+console.log(multiple_by_2(4));
+console.log(divide_by_2(number));
